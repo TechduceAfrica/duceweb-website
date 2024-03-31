29d58d1aee3b426cba28fc0833fd226d8c6220ff
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-03-31 18:46:20 +0100

    refact: added a className to the parent div and added span elements

--- src/app/components/navBar/hamburgerBTN.jsx
+++ src/app/components/navBar/hamburgerBTN.jsx
@@ -1,11 +1,16 @@
+import style from "./navbar.module.css"
 
 export default function HamburgerBtn() {
     return (
-        <>
+        <div className={style.menu__burger}>
             <span>
                 menu
             </span>
-            svg
-        </>
+            <div>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
     )
 }
