17c0a3177daf12bec969c0ad9e609cffb626a368
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-01 18:13:16 +0100

    Update navMenu.jsx

--- src/app/components/navBar/navMenu.jsx
+++ src/app/components/navBar/navMenu.jsx
@@ -1,9 +1,31 @@
+import { menuLinks, socialMediaLinks } from "@/webCopy/navContent"
+import Logo from "../logo"
+import CloseBTN from "./closeBTN"
+import MenuLinks from "./menuLinks"
+import Socials from "./socials"
+import style from "./navbar.module.css"
+import Mode from "./mode"
 
-
-export default function NavMenu() {
+export default function NavMenu({ closeMenu }) {
     return (
-        <div>
-            
+        <div className={`${style.menu__wrapper} container__p`}>
+            <div className={style.top}>
+                <Logo/>
+                <CloseBTN onClick={closeMenu}/>
+            </div>
+            <div className={style.bottom}>
+                <div className={style.menu__link}>
+                    <MenuLinks Menu={menuLinks}/>
+                </div>
+                <div>
+                    <div className={style.social}>
+                        <Socials Menu={socialMediaLinks}/>
+                    </div>
+                    <div>
+                        <Mode/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
