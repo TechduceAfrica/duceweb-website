8b9cd90ecc2df35e870acb6de411cc4be11b71ba
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-01 18:13:02 +0100

    refact: used the react-responsive plugin to set the condition for which logo to display

--- src/app/components/logo/index.jsx
+++ src/app/components/logo/index.jsx
@@ -1,17 +1,42 @@
+'use client'
+import React from "react";
 import Image from "next/image"
+import Link from "next/link";
+import { useMediaQuery } from "react-responsive";
 
 export default function Logo() {
+    const isDarkMode = useMediaQuery({ query: '(prefers-color-scheme: dark)' });
     return (
-        <>
-            <Image 
-                src="/duceweb-white_logo.webp" 
-                alt="Duceweb-logo" 
-                title="Duceweb-logo"
-                height={40.44} 
-                width={253} 
-                priority={true}
-                className="logo"
-            />
-        </>
+        <Link href={`/`} title="Go to Home">
+            {isDarkMode ? <Light/> : <Dark/> }
+        </Link>
+    )
+}
+
+export function Light () {
+    return (
+        <Image 
+            src="/duceweb-white_logo.webp"
+            alt="Duceweb-logo" 
+            title="Duceweb-logo"
+            height={40.44} 
+            width={253} 
+            priority={true}
+            className="logo"
+        />
+    )
+}
+
+export function Dark () {
+    return (
+        <Image 
+            src="/duceweb-black_logo.webp"
+            alt="Duceweb-logo" 
+            title="Duceweb-logo"
+            height={40.44} 
+            width={253} 
+            priority={true}
+            className="logo"
+        />
     )
 }
