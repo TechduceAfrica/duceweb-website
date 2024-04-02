0a66a7fd7242eaacae35362b0566cb99301feed2
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-02 07:25:44 +0100

    Create footerContent.js

---
+++ src/webCopy/footerContent.js
@@ -0,0 +1,29 @@
+export const textCopy = {
+    text: `We don't just deal with codes and pixels. We explore dreams in the digital world.`,
+}
+
+export const quickLinkCopy = [
+    {
+        title: `About`,
+        url: `/about`,
+    },
+    {
+        title: `Services`,
+        url: `/Services`,
+    },
+    {
+        title: `Portfolio`,
+        url: `/portfolio`,
+    },
+    {
+        title: `Contact`,
+        url: `/contactt`,
+    },
+]
+
+export const gInTouchCopy = {
+    address: `91, Obafemi Awolowo Way, Ikeja, Lagos.`,
+    mailOne: `info@ducehost.com`,
+    mailTwo: `ducehost@gmail.com`,
+    phoneNumber: `+234 902 7006 055`,
+}
