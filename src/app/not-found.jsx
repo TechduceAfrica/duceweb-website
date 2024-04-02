751049cec45002911facd93246ce2bf393d1fbc0
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-02 09:40:28 +0100

    Create not-found.jsx

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function NotFoundPage() {
+    return (
+        <div className={styles.main}>
+            <h1>
+                Wetin You Dey Find?
+            </h1>
+        </div>
+    )
+}
