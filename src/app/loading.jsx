07b8e62203f2eda1c49cba88889f1fbbc6506db2
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-02 09:40:33 +0100

    Create loading.jsx

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function LoadingPage() {
+    return (
+        <div className={styles.main}>
+            <h1>
+                Loading
+            </h1>
+        </div>
+    )
+}
