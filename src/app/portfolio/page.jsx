5ea46b5f813262a4a62c2314c049211e8fc61340
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-05 12:12:28 +0100

    refact: changed the tags for SEO reasons

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,18 @@
+import SpotLightBG from "../components/sparkBG/spotLightBG";
+import { PageTitle } from "../components/text";
 
 
 export default function PortfolioPage() {
     return (
-        <div>
+        <main>
+            <section>
+                <PageTitle text={"Featured Works"}/>
+            </section>
+            <section>
+                
+            </section>
+            <SpotLightBG/>
             PortfolioPage
-        </div>
+        </main>
     )
 }
