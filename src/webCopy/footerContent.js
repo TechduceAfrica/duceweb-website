5a1c0681f56efcd9e8e6e08ee1bb0897079ce66b
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-05 12:12:01 +0100

    Update footerContent.js

--- src/webCopy/footerContent.js
+++ src/webCopy/footerContent.js
@@ -23,7 +23,11 @@ export const quickLinkCopy = [
 
 export const gInTouchCopy = {
     address: `91, Obafemi Awolowo Way, Ikeja, Lagos.`,
+    addressLink: `https://www.google.com/maps/place/Techduce/@6.6015158,3.3440085,19.5z/data=!4m14!1m7!3m6!1s0x103b93a039a74653:0x92ddbd2dc372ec0d!2sDucehost+Web+development+and+Hosting!8m2!3d6.5985861!4d3.3424926!16s%2Fg%2F11kxj5kbpy!3m5!1s0x103b92417cbfb8c9:0x59c09703cf836180!8m2!3d6.6035646!4d3.3451606!16s%2Fg%2F11p5qpf6l2?entry=ttu`,
     mailOne: `info@ducehost.com`,
+    mailOneLink: `mailto:info@ducehost.com`,
     mailTwo: `ducehost@gmail.com`,
+    mailTwoLink: `mailto:ducehost@gmail.com`,
     phoneNumber: `+234 902 7006 055`,
+    phoneNumberLink: `tel:2349027006055`,
 }
