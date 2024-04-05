21af010d05cda018e893a8e45782f155e2b0998b
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-05 12:14:51 +0100

    Update index.jsx

--- src/app/components/navBar/index.jsx
+++ src/app/components/navBar/index.jsx
@@ -1,21 +1,21 @@
+'use client'
 import Logo from "../logo";
 import HamburgerBtn from "./hamburgerBTN";
 import NavMenu from "./navMenu";
 import style from "./navbar.module.css"
-
+import { useMenuState } from "./Lib/menuState";
 
 export default function NavBar() {
+    const [isClose, toggleMenu] = useMenuState(false);
     return (
         <header className={`${style.header} container__p`}>
-            <div>
+            <div className="logo__wrapper">
                 <Logo/>
             </div>
-            <div>
+            <div onClick={toggleMenu} >
                 <HamburgerBtn/>
             </div>
-            <NavMenu 
-                // closeMenu={closeMenu}
-            />
+            <NavMenu isClose={isClose} toggleMenu={toggleMenu}/>
         </header>
     )
 }
