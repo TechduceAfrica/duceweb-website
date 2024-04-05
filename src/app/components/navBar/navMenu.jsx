fafd98c6798417caa9ffd455894abbd4604e93a1
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-05 12:16:11 +0100

    Update navMenu.jsx

--- src/app/components/navBar/navMenu.jsx
+++ src/app/components/navBar/navMenu.jsx
@@ -1,31 +1,60 @@
-import { menuLinks, socialMediaLinks } from "@/webCopy/navContent"
-import Logo from "../logo"
+import { motion } from "framer-motion"
 import CloseBTN from "./closeBTN"
 import MenuLinks from "./menuLinks"
 import Socials from "./socials"
-import style from "./navbar.module.css"
 import Mode from "./mode"
+import Logo from "../logo"
+import { menuLinks, socialMediaLinks } from "@/webCopy/navContent"
+import { menuVariant, innerTop, fadeInCenter } from "./Lib/animate"
+import { useMenuState } from "./Lib/menuState"
+import style from "./navbar.module.css"
 
-export default function NavMenu({ closeMenu }) {
+export default function NavMenu({ isClose, toggleMenu }) {
+    // const [isClose, toggleMenu] = useMenuState(false);
     return (
-        <div className={`${style.menu__wrapper} container__p`}>
-            <div className={style.top}>
-                <Logo/>
-                <CloseBTN onClick={closeMenu}/>
+        <motion.div 
+            className={`${style.menu__wrapper} container__p overflow__hidden ${isClose ? `show` : ""}`}
+            variants={menuVariant}
+            initial="closed"
+            animate={isClose ? "open" : "closed"}
+        >
+            <div 
+                className={style.top}
+            >
+                <motion.div 
+                    onClick={toggleMenu}
+                    variants={innerTop}
+                    initial="initial"
+                    animate={isClose ? "enter" : "exit"}
+                    exit="exit"
+                    className="logo__wrapper"
+                >
+                    <Logo/>
+                </motion.div>
+                <CloseBTN toggleMenu={toggleMenu}/>
             </div>
+
             <div className={style.bottom}>
-                <div className={style.menu__link}>
-                    <MenuLinks Menu={menuLinks}/>
+                <div 
+                    className={style.menu__link}
+                >
+                    <MenuLinks Menu={menuLinks} toggleMenu={toggleMenu}/>
                 </div>
-                <div>
+                <motion.div 
+                    className={style.bottom__right}
+                    variants={fadeInCenter}
+                    initial="initial"
+                    animate={isClose ? "enter" : "exit"}
+                    exit="exit"
+                >
                     <div className={style.social}>
                         <Socials Menu={socialMediaLinks}/>
                     </div>
                     <div>
                         <Mode/>
                     </div>
-                </div>
+                </motion.div>
             </div>
-        </div>
+        </motion.div>
     )
 }
