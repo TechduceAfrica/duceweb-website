7829e8d5e12589b3c38a0364ce9233b7bf593b0b
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-05 12:11:02 +0100

    Create index.jsx

---
+++ src/app/components/ctaSection/index.jsx
@@ -0,0 +1,17 @@
+import SpotLightBG from '../sparkBG/spotLightBG'
+import { Body, Title } from '../text'
+import style from "./cta.module.css";
+
+export default function CtaSection() {
+    return (
+        <section className={`${style.sec__wrapper} container__p`}>
+            <SpotLightBG/>
+            <div className={`${style.content__wrapper} rela z__1`}>
+                <Title text={"Got a spark of inspiration?"}/>
+                <Body text={'Drop us a line and let the innovation begin!'}/>
+                <button>Lets chat</button>
+            </div>
+                
+        </section>
+    )
+}
