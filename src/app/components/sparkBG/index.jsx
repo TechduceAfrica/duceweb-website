a1b080d155f01cd91c5299fcecc9ad26d9deaafd
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-05 12:15:43 +0100

    Update index.jsx

--- src/app/components/sparkBG/index.jsx
+++ src/app/components/sparkBG/index.jsx
@@ -1,22 +1,23 @@
 'use client'
-import UseMousePosition from "./lib/useMousePosition";
-import useModeState from "../navBar/Lib/modeState";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import style from  "./spark.module.css";
+import UseMousePosition from "./lib/useMousePosition";
 
 export default function SparkBG() {
 
-    const { isActive } = useModeState();
+    // const { isActive } = useModeState();
     const { x, y }  = UseMousePosition();
     const size = 200;
+
+    // console.log("x", x, "y", y )
 
     return (
         <div className={style.container}>
             <motion.div 
                 className={style.mask}
                 animate={{
-                    WebkitMaskPosition: `${x - (size/2)}px ${y - (size/2)}px`,
+                    WebkitMaskPosition: `${x - (size/2)}% ${y - (size/2)}%`,
                 }}
                 transition={{
                     type: "tween", 
@@ -26,10 +27,12 @@ export default function SparkBG() {
                     mass: 0.1
                 }}
             >
-                <Image src={isActive ? '/bg/line-bg-dark.svg' : '/bg/line-bg-light.svg'} alt={'bg'} width={1440} height={751}/>
+                <Image src={'/bg/line-bg-dark.svg'} alt={'bg'} width={1440} height={751}/>
+                {/* <Image src={isActive ? '/bg/line-bg-dark.svg' : '/bg/line-bg-light.svg'} alt={'bg'} width={1440} height={751}/> */}
             </motion.div>
-            <div>
-                <Image src={isActive ? '/bg/r-line-bg-dark.png' : '/bg/r-line-bg-light.png'} alt={'bg'} width={1440} height={751}/>
+            <div className={style.bg}>
+                <Image src={'/bg/r-line-bg-dark.png'} alt={'bg'} width={1440} height={751}/>
+                {/* <Image src={isActive ? '/bg/r-line-bg-dark.png' : '/bg/r-line-bg-light.png'} alt={'bg'} width={1440} height={751}/> */}
             </div>
         </div>
     )
