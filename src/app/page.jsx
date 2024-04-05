b665b32f49dd6d1eb8bc46ad01ae36e8043f843c
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-05 12:11:29 +0100

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
+import CtaSection from "./components/ctaSection";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      
+    <main className={`${styles.main} no__pe`}>
+      <CtaSection/>
     </main>
   );
 }
