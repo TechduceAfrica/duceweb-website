c787431df3002f73e6c9923c0c2dcd3ed4855e42
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-07 15:30:45 +0100

    refact: code refctor and map function added

--- src/app/components/portfolioComponents/singlePortfolio.jsx
+++ src/app/components/portfolioComponents/singlePortfolio.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import Image from 'next/image'
 import { Body } from '../text'
+import style from "./portfolioComponent.module.css"
+import Link from 'next/link'
 
-export default function SinglePortfolio({ singleProject, text}) {
+export default function SinglePortfolio({ Elements }) {
     return (
-        <div>
-            <div>
-                <Image {...singleProject}/>
-            </div>
-            <div>
-                <Body text={text}/>
-            </div>
+        <div className={style.container}>
+            {Elements.map((element, index ) => 
+                (
+                    <Link 
+                        key={index} 
+                        href={element.link} 
+                        className={style.el}>
+                        <div className={style.wrapper}>
+                            <Image {...element}/>
+                        </div>
+                        <div>
+                            <Body text={element.client}/>
+                        </div>
+                    </Link>
+                )
+            )}
         </div>
     )
 }
