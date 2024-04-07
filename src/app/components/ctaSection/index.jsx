1ec954b8f40f351632fc19a28557d94dae5dd785
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-07 15:24:28 +0100

    refact: comment out the spotLight component

--- src/app/components/ctaSection/index.jsx
+++ src/app/components/ctaSection/index.jsx
@@ -5,7 +5,7 @@ import style from "./cta.module.css";
 export default function CtaSection() {
     return (
         <section className={`${style.sec__wrapper} container__p`}>
-            <SpotLightBG/>
+            {/* <SpotLightBG/> */}
             <div className={`${style.content__wrapper} rela z__1`}>
                 <Title text={"Got a spark of inspiration?"}/>
                 <Body text={'Drop us a line and let the innovation begin!'}/>
