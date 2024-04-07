659e40fa37cdd67a18489d3396d50a5e80badf7c
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-07 15:31:00 +0100

    Update page.jsx

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,18 +1,21 @@
 import SpotLightBG from "../components/sparkBG/spotLightBG";
 import { PageTitle } from "../components/text";
+import SinglePortfolio from "../components/portfolioComponents/singlePortfolio";
+import { mainPage } from "@/webCopy/portfolioContent";
+import CtaSection from "../components/ctaSection";
 
 
 export default function PortfolioPage() {
     return (
         <main>
-            <section>
-                <PageTitle text={"Featured Works"}/>
+            <section className="hero__sec no__pe">
+                <PageTitle text={mainPage.title}/>
             </section>
             <section>
-                
+                <SinglePortfolio Elements={mainPage.elements}/>
             </section>
+            <CtaSection/>
             <SpotLightBG/>
-            PortfolioPage
         </main>
     )
 }
