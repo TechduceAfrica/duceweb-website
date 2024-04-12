2b8f75df339502f31c9349277dd73df0d635a228
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-12 01:56:13 +0100

    Update hero.jsx

--- src/app/components/portfolioComponents/hero.jsx
+++ src/app/components/portfolioComponents/hero.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import { Body, PageTitle } from '../text'
+import style from "./portfolioClient.module.css";
 
 export default function Hero({ title, body }) {
     return (
-        <section>
+        <section className={`${style.hero__sec} container__p`}>
             <PageTitle text={title}/>
             <Body text={body}/>
         </section>
