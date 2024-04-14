749dd3691f2ca26a24eb1515c984925167849abb
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-14 22:56:20 +0100

    Update hero.jsx

--- src/app/components/maintenanceComponents/hero.jsx
+++ src/app/components/maintenanceComponents/hero.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { PageTitle } from "../text";
+import style from "./maintenance.module.css";
 
-export default function Hero() {
+export default function Hero({ topic, subTopic }) {
     return (
-        <section>
-            
+        <section className={`${style.hero__wrapper} container__p`}>
+            <div>
+                <span>
+                    {subTopic}
+                </span>
+                <PageTitle 
+                    text={topic} 
+                />
+            </div>
         </section>
     )
 }
