ab7db6a6f966eb837036a9c43d989fcefdef358b
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-14 22:56:05 +0100

    Update index.jsx

---
+++ src/app/components/maintenanceComponents/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './hero'
+import PriceCard from './priceCard'
+import WhyChooseUs from './whyChooseUs'
+import { tableData } from '@/webCopy/maintenancePageCopy'
+
+export default function MaintenanceComponents({ topic, subTopic, Cards }) {
+    return (
+        <>
+            <Hero subTopic={subTopic} topic={topic}/>
+            <PriceCard Cards={Cards} />
+            <WhyChooseUs tableData={tableData}/>
+        </>
+    )
+}
