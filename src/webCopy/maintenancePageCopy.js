af419a8bcb52e9fc8b13405e964b7c486bc07332
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-14 13:51:57 +0100

    Create maintenancePageCopy.js

---
+++ src/webCopy/maintenancePageCopy.js
@@ -0,0 +1,106 @@
+export const HeroCopy = {
+    topic: `Prevent downtime, enhance security, to keep your website evergreen.`,
+    subTopic: `OUR MAINTENANCE HUB`,
+}
+
+export const Cards = [
+    {
+        title: ``,
+        priceOne: ``,
+        durationOne: ``,
+        priceTwo: ``,
+        durationTwo: ``,
+        planComp: [
+            {
+                listOne: `Robust cybersecurity measures to protect against threats.`,
+                listOneBody: ``,
+                listTwo: `Robust cybersecurity measures to protect against threats.`,
+                listTwoBody: ``,
+                listThree: `Robust cybersecurity measures to protect against threats.`,
+                listThreeBody: ``,
+
+            },
+        ],
+        Benefits: [
+            {
+                list: `Robust cybersecurity measures to protect against threats.`,
+            },
+            {
+                list: `Peace of mind with backups and easy restore options.`,
+            },
+            {
+                list: `Continuous website optimization with regular updates.`,
+            }
+        ]
+    },
+    {
+        title: ``,
+        priceOne: ``,
+        durationOne: ``,
+        priceTwo: ``,
+        durationTwo: ``,
+        planComp: [
+            {
+                listOne: `Robust cybersecurity measures to protect against threats.`,
+                listOneBody: ``,
+                listTwo: `Robust cybersecurity measures to protect against threats.`,
+                listTwoBody: ``,
+                listThree: `Robust cybersecurity measures to protect against threats.`,
+                listThreeBody: ``,
+
+            },
+        ],
+        Benefits: [
+            {
+                list: `Robust cybersecurity measures to protect against threats.`,
+            },
+            {
+                list: `Peace of mind with backups and easy restore options.`,
+            },
+            {
+                list: `Continuous website optimization with regular updates.`,
+            }
+        ]
+    },
+    {
+        title: ``,
+        priceOne: ``,
+        durationOne: ``,
+        priceTwo: ``,
+        durationTwo: ``,
+        planComp: [
+            {
+                listOne: `Robust cybersecurity measures to protect against threats.`,
+                listOneBody: ``,
+                listTwo: `Robust cybersecurity measures to protect against threats.`,
+                listTwoBody: ``,
+                listThree: `Robust cybersecurity measures to protect against threats.`,
+                listThreeBody: ``,
+
+            },
+        ],
+        Benefits: [
+            {
+                list: `Robust cybersecurity measures to protect against threats.`,
+            },
+            {
+                list: `Peace of mind with backups and easy restore options.`,
+            },
+            {
+                list: `Continuous website optimization with regular updates.`,
+            }
+        ]
+    },
+]
+
+// export const Benefits = [
+//     {
+//         list: `Robust cybersecurity measures to protect against threats.`,
+//     },
+//     {
+//         list: `Peace of mind with backups and easy restore options.`,
+//     },
+//     {
+//         list: `Continuous website optimization with regular updates.`,
+//     }
+// ]
