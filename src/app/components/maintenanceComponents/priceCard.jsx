fe83cb59437957dba69e31c597841756d9005d15
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-14 22:57:29 +0100

    Update priceCard.jsx

--- src/app/components/maintenanceComponents/priceCard.jsx
+++ src/app/components/maintenanceComponents/priceCard.jsx
@@ -2,7 +2,7 @@
 import { SubTitle, Title } from "../text";
 import BenefitsList from "./benefits";
 import PlanComponents from "./planComponents";
-
+import style from './maintenance.module.css';
 
 export default function PriceCard({ Cards }) {
 
@@ -11,24 +11,36 @@ export default function PriceCard({ Cards }) {
     }
 
     return (
-        <section>
-            {Cards.map(( card, index ) => {
-                <div key={index}>
-                    <Title text={card.title}/>
-                    <span>{card.priceOne}</span>
-                    <span>{card.durationOne}</span>
-                    <span>{card.priceTwo}</span>
-                    <span>{card.durationTwo}</span>
-                    <div>
-                        <SubTitle text={'Benefits:'}/>
-                        <BenefitsList Benefits={card.Benefits}/>
-                    </div>
-                    <PlanComponents planComp={card.planComp}/>
-                    <button onClick={popUp}>
-                        Choose Plan
-                    </button>
-                </div> 
-            })}
+        <section className={`container__p`}>
+            <div className={style.card__wrapper}>
+                {Cards.map(( card, index ) => (
+                    <div 
+                        key={index} 
+                        className={style.card}
+                    >
+                        <Title text={card.title}/>
+                        <div className={style.price__box}>
+                            <span>{card.priceOne}</span>
+                            <span>{card.durationOne}</span>
+                        </div>
+                        <div className={style.price__box}>
+                            <span>{card.priceTwo}</span>
+                            <span>{card.durationTwo}</span>
+                        </div>
+                        <div className={style.benefit__wrapper}>
+                            <SubTitle text={'Benefits:'}/>
+                            <BenefitsList Benefits={card.Benefits}/>
+                        </div>
+                        <div className={style.component__wrapper}>
+                            <SubTitle text={'Components:'}/>
+                            <PlanComponents planComp={card.planComp}/>
+                        </div>
+                        <button onClick={popUp}>
+                            Choose Plan
+                        </button>
+                    </div> 
+                ))}
+            </div>
         </section>
     )
 }
