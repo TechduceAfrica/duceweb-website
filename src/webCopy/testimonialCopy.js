12c21ab2ac8836404d879c27ff6277de70672faf
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-14 22:55:47 +0100

    Create testimonialCopy.js

---
+++ src/webCopy/testimonialCopy.js
@@ -0,0 +1,50 @@
+export const TestimonialsCopy = [
+    {
+        img: {
+            src: ``,
+            alt: ``,
+            title: ``,
+            width: `1440`,
+            height: `772`,
+        },
+        clientName: `Opeyemi`,
+        barnd: `CEO and Founder, POF Luxury.`,
+        testimony: `"Ducehost's expertise and creativity transformed our online presence, helping us stand out in a crowded market. We're grateful for their invaluable contribution."`,
+    },
+    {
+        img: {
+            src: ``,
+            alt: ``,
+            title: ``,
+            width: `1440`,
+            height: `772`,
+        },
+        clientName: `Daniel Egunjobi`,
+        barnd: `CEO and Founder, Grokbrand.`,
+        testimony: `“They consistently demonstrate attentiveness to our inquiries, offering expert and punctual responses. I wholeheartedly recommend them to anyone seeking to enhance their SEO."`,
+    },
+    {
+        img: {
+            src: ``,
+            alt: ``,
+            title: ``,
+            width: `1440`,
+            height: `772`,
+        },
+        clientName: `Data Adie`,
+        barnd: `CEO and Founder, Data Adie.`,
+        testimony: `“Their service was not only affordable but also professionally executed. They met their promised timeframe, and I was genuinely amazed when I saw my website."`,
+    },
+    {
+        img: {
+            src: ``,
+            alt: ``,
+            title: ``,
+            width: `1440`,
+            height: `772`,
+        },
+        clientName: `Oluwafemi Adekunle`,
+        barnd: `CEO and Founder, Mediglow NG.`,
+        testimony: `“Our experience with DuceHost has been exceptional. They not only deliver tangible results but also exemplify a top-tier agency in terms of customer care."`,
+    },
+]
