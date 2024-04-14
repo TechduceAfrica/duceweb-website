36d156ce39948674b6e461df4422b9faa38ef307
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-14 22:57:15 +0100

    fix: changed [] to {}

--- src/app/components/maintenanceComponents/planComponents.jsx
+++ src/app/components/maintenanceComponents/planComponents.jsx
@@ -1,16 +1,22 @@
 
 export default function PlanComponents({ planComp }) {
 
-    const [listOne, listOneBody, listTwo, listTwoBody, listThree, listThreeBody] = planComp;
+    const { listOne, listOneBody, listTwo, listTwoBody, listThree, listThreeBody } = planComp;
 
     return (
-        <details>
-            <summary>{listOne}</summary>
-            <p>{listOneBody}</p>
-            <summary>{listTwo}</summary>
-            <p>{listTwoBody}</p>
-            <summary>{listThree}</summary>
-            <p>{listThreeBody}</p>
-        </details>
+        <>
+            <details name="accordion">
+                <summary>{listOne}</summary>
+                <ul dangerouslySetInnerHTML={ {__html: listOneBody}}></ul>
+            </details>
+            <details name="accordion">
+                <summary>{listTwo}</summary>
+                <ul dangerouslySetInnerHTML={ {__html: listTwoBody}}></ul>
+            </details>
+            <details name="accordion">
+                <summary>{listThree}</summary>
+                <ul dangerouslySetInnerHTML={ {__html: listThreeBody}}></ul>
+            </details>
+        </>
     )
 }
