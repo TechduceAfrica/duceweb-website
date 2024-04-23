0a015d29595b746cda1f849461f2c01b48391fbe
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-23 11:45:10 +0100

    chores: Added motion to the logo element to make it disappear on page scroll

--- src/app/components/navBar/index.jsx
+++ src/app/components/navBar/index.jsx
@@ -1,17 +1,27 @@
 'use client'
+import { useRef } from "react";
 import Logo from "../logo";
 import HamburgerBtn from "./hamburgerBTN";
 import NavMenu from "./navMenu";
 import style from "./navbar.module.css"
 import { useMenuState } from "./Lib/menuState";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function NavBar() {
     const [isClose, toggleMenu] = useMenuState(false);
+    const logoRef = useRef(null)
+    const { scrollY } = useScroll(
+        {
+            target: logoRef,
+            offset: ["start end", "end end"]
+        }
+    );
+    const size = useTransform(scrollY, [0, 0.2], [1 , 0])
     return (
         <header className={`${style.header} container__p`}>
-            <div className="logo__wrapper">
+            <motion.div style={{ scale: size}} className="logo__wrapper" ref={logoRef}>
                 <Logo/>
-            </div>
+            </motion.div>
             <div onClick={toggleMenu} className={style.menu__btn} >
                 <HamburgerBtn/>
             </div>
