6f5850c70ae5e95de4f564488de4fce03fc856eb
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-23 14:45:31 +0100

    chores: added the Testimonial component to the maintenance page

--- src/app/components/maintenanceComponents/index.jsx
+++ src/app/components/maintenanceComponents/index.jsx
@@ -3,6 +3,7 @@ import Hero from './hero'
 import PriceCard from './priceCard'
 import WhyChooseUs from './whyChooseUs'
 import { tableData } from '@/webCopy/maintenancePageCopy'
+import Testimonial from '../testimonial'
 
 export default function MaintenanceComponents({ topic, subTopic, Cards }) {
     return (
@@ -10,6 +11,7 @@ export default function MaintenanceComponents({ topic, subTopic, Cards }) {
             <Hero subTopic={subTopic} topic={topic}/>
             <PriceCard Cards={Cards} />
             <WhyChooseUs tableData={tableData}/>
+            <Testimonial/>
         </>
     )
 }
