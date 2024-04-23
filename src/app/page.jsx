38bb778934eccea76a9b446b85e9775e694faca3
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-23 14:47:13 +0100

    chore: added the testimonial component to the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import CtaSection from "./components/ctaSection";
+import Testimonial from "./components/testimonial";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={`${styles.main} no__pe`}>
+      <Testimonial/>
       <CtaSection/>
     </main>
   );
