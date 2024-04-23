6174fc30ae5e6104ca905b00c3c80ae33a7cd535
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-23 15:19:54 +0100

    Create aboutPageCopy.js

---
+++ src/webCopy/aboutPageCopy.js
@@ -0,0 +1,80 @@
+export const hero = {
+    title: `DuceWeb: Our Story`,
+    desc: `DuceWeb is a modern part of TechDuce Africa. We tell our own story in the digital world, going beyond the usual Cinderella  fairy tale. Our journey is based on a big idea, and it's like an interesting story with each part having new and bold ideas and very clever work.`,
+}
+
+export const aboutImg = {
+    src: `/images/About-DuceWeb-Intro.webp`,
+    alt: `DuceWeb is a modern part of TechDuce Africa`,
+    title: `DuceWeb is a modern part of TechDuce Africa`,
+    width: `1440`,
+    height: `680`,
+}
+
+export const approach = {
+    title: `Our Approach`,
+    cards: [
+        {
+            topic: `Ideate`,
+            img: {
+                src: `/images/Ideate-icon.png`,
+                alt: `DuceWeb Approach - Ideate`,
+                title: `DuceWeb Approach - Ideate`,
+                width: `130`,
+                height: `130`,
+            },
+            body: `This is where the <span>magic starts! </span> It's the moment we gather <span>thoughts, </span> imagine <span>possibilities, </span> and shape <span>an overall concept.</span>`,
+        },
+        {
+            topic: `Design`,
+            img: {
+                src: `/images/Design-icon.png`,
+                alt: `DuceWeb Approach - Design`,
+                title: `DuceWeb Approach - Design`,
+                width: `130`,
+                height: `130`,
+            },
+            body: `After we've got our ideas, it's time to make them look good and work smoothly to provide a functional digital product.`,
+        },
+        {
+            topic: `Develop`,
+            img: {
+                src: `/images/Develop-icon.png`,
+                alt: `DuceWeb Approach - Develop`,
+                title: `DuceWeb Approach - Develop`,
+                width: `130`,
+                height: `130`,
+            },
+            body: `Now that you have a plan and a design, it's time to bring your digital product to life!`,
+        },
+        {
+            topic: `Maintain`,
+            img: {
+                src: `/images/Maintain-icon.png`,
+                alt: `DuceWeb Approach - Maintain`,
+                title: `DuceWeb Approach - Maintain`,
+                width: `130`,
+                height: `130`,
+            },
+            body: `Your digital product is live! We ensure smooth operation, and occasionally add new features or enhancements.`,
+        },
+    ]
+}
+
+export const wwdo = {
+    title: ``,
+    cards: [
+        {
+            title: `Mobile App Development`,
+            desc: ``,
+            img: {
+                src: `/images/Maintain-icon.png`,
+                alt: `DuceWeb Approach - Maintain`,
+                title: `DuceWeb Approach - Maintain`,
+                width: `130`,
+                height: `130`,
+            },
+            color: ``,
+        }
+    ]
+}
