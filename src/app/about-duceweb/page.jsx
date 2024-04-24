2e545b8c095aab2049a22a7888ca61fe96f8008b
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-24 12:37:50 +0100

    refact: added the about page component

--- src/app/about-duceweb/page.jsx
+++ src/app/about-duceweb/page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import AboutPageComponents from '../components/aboutComponents'
 
 export default function About() {
     return (
         <main>
-            About page
+            <AboutPageComponents/>
         </main>
     )
 }
