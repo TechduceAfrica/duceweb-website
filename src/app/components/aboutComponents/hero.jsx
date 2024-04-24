e8c443b6d92b8459e71deed64fe3cb662b06881b
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-24 12:41:23 +0100

    chores: added props

--- src/app/components/aboutComponents/hero.jsx
+++ src/app/components/aboutComponents/hero.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Body, PageTitle } from '../text'
+import style from './about.module.css'
 
-export default function Hero() {
+export default function Hero({ title, desc }) {
     return (
-        <section>
-            
+        <section className='container__p'>
+            <div className={style.hero__wrapper}>
+                <PageTitle text={title}/>
+                <Body text={desc}/>
+            </div>
         </section>
     )
 }
